ce7a5df98e6a51f284ac8dda6649cbe657de7de8
Octo <106983660+OctoSplash01@users.noreply.github.com>
2022-12-30 12:16:33 +0100

    ghost blocks because gabagool reverted the update?

--- src/main/java/me/aurora/client/features/dungeons/Ghostblock.java
+++ src/main/java/me/aurora/client/features/dungeons/Ghostblock.java
@@ -1,25 +1,39 @@
-package me.aurora.client.features.dungeons;
+package me.dailydungeons.client.features;
 
-import me.aurora.client.config.Config;
+import me.dailydungeons.client.config.Config;
+import me.dailydungeons.client.events.ClickEvent;
+import me.dailydungeons.client.events.DailyDungeonsMessages;
+import me.dailydungeons.client.utils.SkyBlockID;
+import me.dailydungeons.client.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.lwjgl.input.Keyboard;
 
 
-public class    Ghostblock {
-    Minecraft mc = Minecraft.getMinecraft();
-    
-    //po co ty to zmieniales, tak to trzeba caly czas klikac zamiast normalnie trzymac
+public class Ghostblock {
 
-    @SubscribeEvent
-    public void onTick(TickEvent.PlayerTickEvent event) {
-        if(Keyboard.isKeyDown(Keyboard.KEY_G) && Config.ghostblock) {
-            MovingObjectPosition POSITIONOFBLOCK = mc.thePlayer.rayTrace(mc.playerController.getBlockReachDistance(), 1);
-                mc.theWorld.setBlockToAir(POSITIONOFBLOCK.getBlockPos());
-        }
-    }
+
+    Minecraft mc = Minecraft.getMinecraft();
+
+
+
+  /*  @SubscribeEvent
+    public void onRenderWorld(RenderWorldLastEvent event) {
+        if (mc.gameSettings.isKeyDown(key))
+
+
+    } */
+
+  @SubscribeEvent
+  public void onTick(TickEvent.PlayerTickEvent event) {
+      if(Keyboard.isKeyDown(Keyboard.KEY_G) && Config.ghostblock) {
+          MovingObjectPosition POSITIONOFBLOCK = mc.thePlayer.rayTrace(mc.playerController.getBlockReachDistance(), 1);
+          mc.theWorld.setBlockToAir(POSITIONOFBLOCK.getBlockPos());
+      }
+  }
 
 }
 
