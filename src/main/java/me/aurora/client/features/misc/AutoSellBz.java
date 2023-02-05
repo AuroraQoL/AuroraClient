aef05dcdb4643cab3db694cc8880a2abaa8af644
Octo <106983660+OctoSplash01@users.noreply.github.com>
2023-02-05 20:03:03 +0100

    AutoBazaarSell
    i am boring

---
+++ src/main/java/me/aurora/client/features/misc/AutoSellBz.java
@@ -0,0 +1,55 @@
+package me.aurora.client.features.misc;
+
+import me.aurora.client.config.Config;
+import me.aurora.client.utils.ClientMessages;
+import me.aurora.client.utils.InventoryUtils;
+import me.aurora.client.utils.conditions.Conditions;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import static me.aurora.client.Aurora.mc;
+
+public class AutoSellBz {
+
+    private int delay = 0;
+    private boolean inBazaar = false;
+    private boolean areYouSure = false;
+
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent event) {
+        if(Config.autoSellBz && event.type == 0) {
+            String message = event.message.getFormattedText().replaceAll("\u00a7.", "");
+            if(message.equals("Your inventory is full!")) {
+                mc.thePlayer.sendChatMessage("/bz");
+                ClientMessages.sendClientMessage("Selling Items on Bazaar...");
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onBackgroundRender(GuiScreenEvent.BackgroundDrawnEvent event) {
+        String chestName = InventoryUtils.getGuiName(event.gui);
+        inBazaar = chestName.contains("Bazaar");
+        areYouSure = chestName.equals("Are you sure?");
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent event) {
+        if (delay % 5 == 0) {
+            if (mc.currentScreen instanceof GuiChest) {
+                if (inBazaar && Config.autoSellBz) {
+                    mc.playerController.windowClick(mc.thePlayer.openContainer.windowId, 48, 2, 3, mc.thePlayer);
+                }
+                else if (areYouSure && Config.autoSellBz) {
+                    mc.playerController.windowClick(mc.thePlayer.openContainer.windowId, 12, 2, 3, mc.thePlayer);
+                }
+            }
+        }
+        delay++;
+    }
+}
