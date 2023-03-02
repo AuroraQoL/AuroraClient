bcaddb75dab02126d6a01b31108d31dee8dc4b22
Octo <106983660+OctoSplash01@users.noreply.github.com>
2023-03-02 15:37:53 +0100

    vclip vclip vclip BOOM BOOM BOOM

--- src/main/java/me/aurora/client/features/movement/VClip.java
+++ src/main/java/me/aurora/client/features/movement/VClip.java
@@ -2,6 +2,7 @@ package me.aurora.client.features.movement;
 
 import me.aurora.client.config.Config;
 import me.aurora.client.features.Module;
+import me.aurora.client.utils.BindUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
@@ -19,7 +20,7 @@ public class VClip implements Module {
 
     @SubscribeEvent
     public void onKeyPress(InputEvent.KeyInputEvent event) {
-        if(Keyboard.isKeyDown(Keyboard.KEY_K) && Config.verticalClip) {
+        if(BindUtils.getBindStatusPressed("VerticalClip") && Config.verticalClip) {
             mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY - Config.verticalClip_ParameterDistance, mc.thePlayer.posZ);
         }
     }
