d66092cc951794ca3fef3261c249cabc5c64ffa0
Gabagooooooooooool <116589326+Gabagooooooooooool@users.noreply.github.com>
2023-03-05 19:12:15 +0100

    Fixed AutoSprint
    @OctoSplash01 stop releasing broken features

--- src/main/java/me/aurora/client/features/movement/AutoSprint.java
+++ src/main/java/me/aurora/client/features/movement/AutoSprint.java
@@ -3,6 +3,7 @@ package me.aurora.client.features.movement;
 import me.aurora.client.config.Config;
 import me.aurora.client.features.Module;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -22,14 +23,14 @@ public class AutoSprint  implements Module {
     public void LivingUpdateEvent(LivingEvent.LivingUpdateEvent event) {
         if (Config.autoSprintSettings == 0) {
             if (event.entityLiving instanceof EntityPlayer) {
-                if (Config.autoSprint && Minecraft.getMinecraft().thePlayer.onGround && !mc.thePlayer.isSneaking() && !mc.thePlayer.isUsingItem() && (GameSettings.isKeyDown(mc.gameSettings.keyBindForward))) {
+                if (Config.autoSprint && Minecraft.getMinecraft().thePlayer.onGround && !(mc.currentScreen instanceof GuiChat) && !mc.thePlayer.isSneaking() && !mc.thePlayer.isUsingItem() && (GameSettings.isKeyDown(mc.gameSettings.keyBindForward))) {
                     mc.thePlayer.setSprinting(true);
                 }
             }
         }
         if (Config.autoSprintSettings == 1) {
             if (event.entityLiving instanceof EntityPlayer) {
-                if (Config.autoSprint && (GameSettings.isKeyDown(mc.gameSettings.keyBindRight) || GameSettings.isKeyDown(mc.gameSettings.keyBindLeft) || GameSettings.isKeyDown(mc.gameSettings.keyBindForward) || GameSettings.isKeyDown(mc.gameSettings.keyBindBack))) {
+                if (Config.autoSprint && !(mc.currentScreen instanceof GuiChat) && !mc.thePlayer.isSneaking() && !mc.thePlayer.isUsingItem() && (GameSettings.isKeyDown(mc.gameSettings.keyBindRight) || GameSettings.isKeyDown(mc.gameSettings.keyBindLeft) || GameSettings.isKeyDown(mc.gameSettings.keyBindForward) || GameSettings.isKeyDown(mc.gameSettings.keyBindBack))) {
                     mc.thePlayer.setSprinting(true);
                 }
             }
