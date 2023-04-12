bb4ae245da0a336e9971d33f0674a279990dd9c7
Octo <106983660+OctoSplash01@users.noreply.github.com>
2023-04-12 12:56:46 +0200

    Improved GhostBlocks

--- src/main/java/me/aurora/client/features/dungeons/Ghostblock.java
+++ src/main/java/me/aurora/client/features/dungeons/Ghostblock.java
@@ -3,9 +3,13 @@ package me.aurora.client.features.dungeons;
 import me.aurora.client.config.Config;
 import me.aurora.client.features.Module;
 import me.aurora.client.utils.BindUtils;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.*;
 
 import static me.aurora.client.Aurora.mc;
 
@@ -25,11 +29,23 @@ public class Ghostblock  implements Module {
         return Config.ghostblocks;
     }
 
+    private final ArrayList<Block> dontGhost = new ArrayList<>(Arrays.asList(
+            Blocks.chest, //secret chests
+            Blocks.lever, //secret levers
+            Blocks.command_block, //terminals
+            Blocks.skull, //wither essence
+            Blocks.bed, //if someone plays bedwars with aurora for whatever reason XD
+            Blocks.trapped_chest)); //mimic (mimic is a mob that is hidden in a trapped chest)
+
     @SubscribeEvent
     public void onTick(TickEvent.PlayerTickEvent event) {
-        if(BindUtils.isBindDown("GhostBlocks") && toggled() && ((System.currentTimeMillis() - lastUsed) > (Config.ghostblocks_delay * 1000))) {
-            lastUsed = System.currentTimeMillis();
-            mc.theWorld.setBlockToAir(mc.thePlayer.rayTrace(Config.ghostblocks_range, 1).getBlockPos());
+        if (BindUtils.isBindDown("GhostBlocks") && toggled() && ((System.currentTimeMillis() - lastUsed) > (Config.ghostblocks_delay * 1000))) {
+            MovingObjectPosition blockPos = mc.thePlayer.rayTrace(Config.ghostblocks_range, 1);
+            Block currentBlock = mc.theWorld.getBlockState(blockPos.getBlockPos()).getBlock();
+            if (!dontGhost.contains(currentBlock)) {
+                lastUsed = System.currentTimeMillis();
+                mc.theWorld.setBlockToAir(blockPos.getBlockPos());
+            }
         }
     }
 }
