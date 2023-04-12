b1510d21707939d44aa43025d2bbc5c59051081a
Octo <106983660+OctoSplash01@users.noreply.github.com>
2023-04-12 12:56:11 +0200

    Improved Auto Rogue

--- src/main/java/me/aurora/client/features/movement/AutoRogue.java
+++ src/main/java/me/aurora/client/features/movement/AutoRogue.java
@@ -4,11 +4,13 @@ import me.aurora.client.config.Config;
 import me.aurora.client.features.Module;
 import me.aurora.client.utils.MessageUtils;
 import me.aurora.client.utils.ItemUtils;
+import me.aurora.client.utils.Timer;
 import me.aurora.client.utils.conditions.ConditionUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import static me.aurora.client.Aurora.mc;
 
@@ -26,24 +28,27 @@ public class AutoRogue implements Module {
     public boolean toggled() {
         return Config.rogueSwordAura;
     }
+
+    Timer timer = new Timer();
+
     @SubscribeEvent
-    public void onInteract(PlayerInteractEvent event) {
-        if (toggled() && ConditionUtils.inSkyblock() && mc.thePlayer.inventory.currentItem == 0 && event.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) {
+    public void onTick(TickEvent.PlayerTickEvent event) {
+        if (toggled() && ConditionUtils.inSkyblock() && mc.thePlayer.inventory.currentItem == 0) {
             for (int i = 0; i < 8; i++) {
                 ItemStack item = mc.thePlayer.inventory.getStackInSlot(i);
-                String itemID = ItemUtils.getSkyBlockID(item);
-                if ((itemID.equals("ROGUE_SWORD"))) {
-                    event.setCanceled(true);
-                    mc.thePlayer.inventory.currentItem = i;
-                    mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, item);
-                    mc.thePlayer.inventory.currentItem = 0;
-                    MessageUtils.sendClientMessage("Used Rogue Sword");
-                    break;
+                if (ItemUtils.getSkyBlockID(item).matches("ROGUE_SWORD")) {
+                    if (timer.timeBetween(30000, true)) {
+                        event.setCanceled(true);
+                        mc.thePlayer.inventory.currentItem = i;
+                        mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, item);
+                        mc.thePlayer.inventory.currentItem = 0;
+                        break;
+                    }
                 }
             }
         }
     }
-    }
+}
 
 
 
