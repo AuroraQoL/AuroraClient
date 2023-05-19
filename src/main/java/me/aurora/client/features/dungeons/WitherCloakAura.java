124ce286e43b75ca87714c5e6687beec9e2a478e
Octo <106983660+OctoSplash01@users.noreply.github.com>
2023-05-19 19:50:45 +0200

    wr
    wr

--- src/main/java/me/aurora/client/features/dungeons/WitherCloakAura.java
+++ src/main/java/me/aurora/client/features/dungeons/WitherCloakAura.java
@@ -4,9 +4,7 @@ import me.aurora.client.config.Config;
 import me.aurora.client.features.Module;
 import me.aurora.client.utils.ItemUtils;
 import me.aurora.client.utils.conditions.ConditionUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -32,15 +30,19 @@ public class WitherCloakAura implements Module {
     @SubscribeEvent
     public void onTick(TickEvent.PlayerTickEvent event) {
         if (toggled() && ConditionUtils.inSkyblock() && mc.thePlayer.isInLava() && mc.thePlayer.inventory.currentItem == 0) {
-            for (int i = 0; i < 8; i++) {
-                ItemStack item = mc.thePlayer.inventory.getStackInSlot(i);
-                if (ItemUtils.getSkyBlockID(item).matches("WITHER_CLOAK_SWORD")) {
-                    event.setCanceled(true);
-                    mc.thePlayer.inventory.currentItem = i;
-                    mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, item);
-                    mc.thePlayer.inventory.currentItem = 0;
-                    break;
-                }
+            useItem("WITHER_CLOAK_SWORD");
+        }
+    }
+
+    public static void useItem(String itemId) {
+        for(int i = 0; i < 8; i++) {
+            ItemStack item = mc.thePlayer.inventory.getStackInSlot(i);
+            if(itemId.equals(ItemUtils.getSkyBlockID(item))) {
+                int previousItem = mc.thePlayer.inventory.currentItem;
+                mc.thePlayer.inventory.currentItem = i;
+                mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, item);
+                mc.thePlayer.inventory.currentItem = previousItem;
+                return;
             }
         }
     }
